b973916e30d8402ee304ece3202b543662d95308
Yoshiaki Ono <ono.kirin@gmail.com>
2018-07-02 15:48:02 +0900

    fix bug

--- kanichart/__init__.py
+++ kanichart/__init__.py
@@ -4,6 +4,6 @@ __version__ = '0.1.2'
 __author__ = 'fx-kirin'
 __all__ = []
 
-from . _chart import CandlestickCharts
-from . _chart import LineCharts
-from . _chart import StockChart
+from ._chart import CandlestickCharts
+from ._chart import LineCharts
+from ._chart import StockChart
