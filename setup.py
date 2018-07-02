6feac9f697e99dba26da478c88af2847e00f5262
Yoshiaki Ono <ono.kirin@gmail.com>
2018-07-02 15:05:12 +0900

    version 0.1.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 
 setuptools.setup(
     name="kanichart",
-    version="0.1.1",
+    version="0.1.2",
     url="https://github.com/fx-kirin/kanichart",
 
     author="Yoshiaki Ono",
